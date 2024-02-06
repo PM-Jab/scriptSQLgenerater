a72db3d5ef860935294846a28f29e65844873d7c
Possawee Muttisan <possawee.m@arise.tech>
2024-02-06 17:35:33 +0700

    fix: gen uuid and add double qoute

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module scriptGenerate
 go 1.21.6
 
 require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/google/uuid v1.6.0 // indirect
--- go/sqlscript/sqlscript.go
+++ go/sqlscript/sqlscript.go
@@ -4,6 +4,8 @@ import (
 	"encoding/hex"
 	"fmt"
 	"os"
+
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -49,17 +51,19 @@ func main() {
 }
 
 func formatMockBranch(num int) string {
-	branch_sk := fmt.Sprintf(`UNHEX(REPLACE("4f3c4f5b-c5b4-46f8-ba2b-e7ab0d14%s","-",""))`, formatDigit(num))
+	u1 := uuid.New()
+	branch_sk := fmt.Sprintf(`UNHEX(REPLACE("%s","-",""))`, u1)
 	branch_id := formatDigit(num)
 	name := fmt.Sprintf("NEW MOCKS 2 สาขาที่ %d", num)
 	number := formatDigit(num)
 
-	text := fmt.Sprintf(`INSERT INTO branch(branch_sk, merchant_id,branch_id, name, number, branch_province_code, branch_mobile, address_no, building, street, moo, soi, sub_district, sub_district_code, district,district_code, province, zip_code, latitude, longitude, created_date, created_by,updated_date,updated_by,is_update_location,last_location_date,tour_company_name,tour_package_name,name_en,last_branch_name_updated_date,use_same_address_for_delivery_address,use_same_address_for_tax_address,vat_branch_code) VALUES ( %s,"W202411151038589998r", %s, %s, %s, "010", "omrt3cyg4eXJAm4ZcuM6Vd72a03akTbds7Q=", "77/782", "99", "ถนน2", "หมู่2", "ซอย2", "แพรกษา", "10140100", "แพรกษา2", "1014", "กรุงเทพมหานคร", "10110", "15.877076727392777", "100.99253810942888", now(), null, now(), "10007", "Y", now(), null, null, "", now(), 0, null, null)\n`, string(branch_sk), branch_id, name, number)
+	text := fmt.Sprintf(`INSERT INTO branch(branch_sk, merchant_id,branch_id, name, number, branch_province_code, branch_mobile, address_no, building, street, moo, soi, sub_district, sub_district_code, district,district_code, province, zip_code, latitude, longitude, created_date, created_by,updated_date,updated_by,is_update_location,last_location_date,tour_company_name,tour_package_name,name_en,last_branch_name_updated_date,use_same_address_for_delivery_address,use_same_address_for_tax_address,vat_branch_code) VALUES ( %s,"W202411151038589998r", "%s", "%s", "%s", "010", "omrt3cyg4eXJAm4ZcuM6Vd72a03akTbds7Q=", "77/782", "99", "ถนน2", "หมู่2", "ซอย2", "แพรกษา", "10140100", "แพรกษา2", "1014", "กรุงเทพมหานคร", "10110", "15.877076727392777", "100.99253810942888", now(), null, now(), "10007", "Y", now(), null, null, "", now(), 0, null, null)`, string(branch_sk), branch_id, name, number)
 	return text
 }
 
 func formatMockMerchant_x_branch(num int) string {
-	branch_sk := fmt.Sprintf(`UNHEX(REPLACE("4f3c4f5b-c5b4-46f8-ba2b-e7ab0d14%s","-",""))`, formatDigit(num))
+	u1 := uuid.New()
+	branch_sk := fmt.Sprintf(`UNHEX(REPLACE("%s","-",""))`, u1)
 
 	text := fmt.Sprintf(`INSERT INTO merchant_x_branch(merchant_id,branch_sk,mobile_no,created_date,created_by,updated_date,updated_by) VALUES ("W202411151038589998r", %s,"omrt3cyg4eXJAm4ZcuM6Vd72a03akTbds7Q=", now(), "10007", now(), "10007")`, branch_sk)
 	return text
